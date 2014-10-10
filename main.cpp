f4f41d4dcd7c0b20ac2b1084706c4a1017ab821e
Alina Saalfeld <alina.saalfeld@ymail.com>
2014-10-10 18:51:00 +0200

    Two windows option added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ extern "C" {
 
 static sf::Clock m_time;
 static uint frames;
+static bool split;
 
 void toRGBA(const uint8_t* rgb, uint8_t* rgba, const size_t count) {
     //printf("Hallo");
@@ -25,15 +26,21 @@ int main()
         capture_deinit();
         return -1;
     }
+    split = false;
     frames = 0;
     m_time = sf::Clock();
     sf::RenderWindow window(sf::VideoMode(400, 480), "Cute 3DS Capture");//,sf::Style::Close);
+    sf::RenderWindow bottom_window;
     sf::RectangleShape top_screen(sf::Vector2f(240,400));
     top_screen.rotate(-90);
     top_screen.setPosition(0,240);
     sf::RectangleShape bottom_screen(sf::Vector2f(240,320));
     bottom_screen.rotate(-90);
     bottom_screen.setPosition(40,480);
+
+    sf::View total = sf::View(sf::FloatRect(0, 0, 400, 480));
+    sf::View top = sf::View(sf::FloatRect(0, 0, 400, 240));
+    sf::View bottom = sf::View(sf::FloatRect(40, 240, 320, 240));
 
 
     sf::Texture texture;
@@ -56,22 +63,33 @@ int main()
             case sf::Event::KeyPressed:
                 switch(event.key.code) {
                 case sf::Keyboard::Num1:
-                    window.setSize(sf::Vector2u(400, 480));
+                    window.setSize(sf::Vector2u(400, 240*(2-int(split))));
                     break;
                 case sf::Keyboard::Num2:
-                    window.setSize(sf::Vector2u(600, 720));
+                    window.setSize(sf::Vector2u(600, 360*(2-int(split))));
                     break;
                 case sf::Keyboard::Num3:
-                    window.setSize(sf::Vector2u(800, 960));
+                    window.setSize(sf::Vector2u(800, 480*(2-int(split))));
                     break;
                 case sf::Keyboard::Num4:
-                    window.setSize(sf::Vector2u(1000, 1200));
+                    window.setSize(sf::Vector2u(1000, 600*(2-int(split))));
                     break;
                 case sf::Keyboard::Num5:
-                    window.setSize(sf::Vector2u(1200, 1680));
+                    window.setSize(sf::Vector2u(1200, 840*(2-int(split))));
                     break;
                 case sf::Keyboard::Space:
-                    //toggel double window
+                    if (!split) {
+                        window.setView(top);
+                        window.setSize(sf::Vector2u(400, 240));
+                        bottom_window.create(sf::VideoMode(320, 240), "Bottom Screen Cute 3DS Capture");
+                        bottom_window.setView(bottom);
+                        split = true;
+                    } else {
+                        window.setView(total);
+                        window.setSize(sf::Vector2u(400, 480));
+                        bottom_window.close();
+                        split = false;
+                    }
                     break;
                 default:
                     break;
@@ -83,16 +101,27 @@ int main()
 
         }
 
+        if (bottom_window.isOpen()) {
+            //printf("open\n");
+        }
+
         frames++;
         if (m_time.getElapsedTime().asMilliseconds() > 250) {
             char title[40];
-            sprintf(title,"Cute 3DS Capture (%.2f FPS)", float(frames)/(m_time.getElapsedTime().asSeconds()));
+            if (!split) {
+                sprintf(title,"Cute 3DS Capture (%.2f FPS)", float(frames)/(m_time.getElapsedTime().asSeconds()));
+            } else {
+                sprintf(title,"Top Screen Cute 3DS Capture (%.2f FPS)", float(frames)/(m_time.getElapsedTime().asSeconds()));
+            }
             m_time.restart();
             frames = 0;
             window.setTitle(title);
         }
 
         window.clear();
+        if(split) {
+            bottom_window.clear();
+        }
 
         uint8_t frameBuf[FRAMEBUFSIZE];
         uint8_t rgbaBuf[FRAMESIZE*4/3];
@@ -116,7 +145,12 @@ int main()
 
         error:
         window.draw(top_screen);
-        window.draw(bottom_screen);
+        if (!split) {
+            window.draw(bottom_screen);
+        } else {
+            bottom_window.draw(bottom_screen);
+            bottom_window.display();
+        }
         window.display();
     }
     capture_init();
