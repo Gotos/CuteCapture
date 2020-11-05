9a2b1281dfb7c4ae87d735f4b4688cca830cbda1
Alina Saalfeld <alina.saalfeld@ymail.com>
2020-11-05 11:09:30 +0100

    move version generation completely to get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-if [ -z "$CC_VERSION" ]
-then
+if [ -z ${GITHUB_REF} ]; then
       printf 'dev-version'
 else
-      printf '%s' "$CC_VERSION"
+  REF_END="${GITHUB_REF#refs/}"
+  REF_MID="${REF_END%%/*}"
+  if [ "$REF_MID" == "tags" ]; then
+    printf "%s" "${GITHUB_REF#refs/tags/}"
+  else
+    printf "dev-%s" "${GITHUB_SHA}"
+  fi
 fi
